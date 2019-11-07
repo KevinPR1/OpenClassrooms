07abf2bda090bed578a43401898fde2d7731cb0d
Kevin Preira <kevinpreira.p@gmail.com>
2019-11-07 10:31:36 +0100

    Correction des fautes de frappe pour les tests Unitaires + layout pour satisfaire android studio sur la symétrie

--- Android/Entrevoisins/app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
+++ Android/Entrevoisins/app/src/test/java/com/openclassrooms/entrevoisins/service/NeighbourServiceTest.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Unit test on Neighbour service
@@ -58,8 +59,8 @@ public class NeighbourServiceTest {
 
     @Test
     public void addFavoriteNeighbour() {
-        Neighbour neighbourToDelete = service.getNeighbours().get(0);
-        service.deleteNeighbour(neighbourToDelete);
-        assertFalse(service.getNeighbours().contains(neighbourToDelete));
+        Neighbour neighbourToadd = service.getNeighbours().get(0);
+        service.addFavoriteNeighbour(neighbourToadd);
+        assertTrue(service.getFavoritesNeighbours().contains(neighbourToadd));
     }
 }
